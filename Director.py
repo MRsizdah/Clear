623bd628646aec183e22be8edcd9d73e38f28a87
MRsizdah <80194479+MRsizdah@users.noreply.github.com>
2021-04-04 13:22:08 -0400

    simple-virus-by-python

---
+++ Director.py
@@ -0,0 +1,72 @@
+import os 
+import glob
+from colorama import Fore , init
+from pyfiglet import Figlet
+
+#banner
+banner = Figlet()
+os.system("cls")
+#banner print
+print(Fore.RED + banner.renderText('CLEAR') )
+print(Fore.RED + ">MR XIII--------------------v.1<")
+
+
+
+#Question
+print(Fore.RED + 'Which format do you decide to delete? ')
+print(Fore.WHITE + '[1] .mp3' )
+print(Fore.WHITE + '[2] .psd' )
+print(Fore.WHITE + '[3] .txt' )
+print(Fore.WHITE + "[4] other format's " )
+print(Fore.RED + "<------------------------------>")
+#User selection
+print(Fore.WHITE  )
+suffixNum = int(input("Enter the format Number ==> "))
+
+
+
+
+if suffixNum == 1:
+    #Clears mp3 files
+    listt = glob.glob("*.mp3")
+    init()
+    if listt:
+        for file in listt:
+            os.remove( file  )
+            print(Fore.GREEN + "[+]  " + Fore.GREEN + file  )
+    else:
+        print(Fore.RED + "The file does not exist in such a format")
+elif suffixNum == 2:
+    #Clears psd files
+    listt = glob.glob("*.psd")
+    init()
+    if listt:
+        for file in listt:
+            os.remove( file  )
+            print(Fore.GREEN + "[+]  " + Fore.GREEN + file  )
+    else:
+        print(Fore.RED + "The file does not exist in such a format")
+elif suffixNum == 3:
+    #Clears txt files
+    listt = glob.glob("*.txt")
+    init()
+    if listt:
+        for file in listt:
+            os.remove( file  )
+            print(Fore.GREEN + "[+]  " + Fore.GREEN + file  )
+    else:
+        print(Fore.RED + "The file does not exist in such a format")
+elif suffixNum == 4:
+    print(Fore.RED + "<------------------------------>")
+    #User selection
+    print(Fore.WHITE  )
+    suffix = input("Enter you Suffix for example 'mp3' ==> ")
+
+    listt = glob.glob("*."+suffix)
+    init()
+    if listt:
+        for file in listt:
+            os.remove( file  )
+            print(Fore.GREEN + "[+]  " + Fore.GREEN + file  )
+    else:
+        print(Fore.RED + 'The file does not exist in such a format')
